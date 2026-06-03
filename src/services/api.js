db5a29860efb916ed12743237d85625b4bbca099
RuchaSinkar <ruchasinkar15@gmail.com>
2026-06-03 12:25:02 +0530

    modified api.js and added dockerfile

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
 
 // Axios instance - automatically attaches JWT token to every request
 const api = axios.create({ baseURL: BASE_URL });
